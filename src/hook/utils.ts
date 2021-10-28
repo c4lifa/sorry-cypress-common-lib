7677b0d0100784849e6d86052c23f24cdc13da73
c4lifa <narutobestclubcom@gmail.com>
2021-10-29 00:34:09 +0300

    initial

---
+++ src/hook/utils.ts
@@ -0,0 +1,28 @@
+import {
+  BitBucketHook,
+  GenericHook,
+  GithubHook,
+  Hook,
+  SlackHook,
+  TeamsHook,
+} from './types';
+
+export function isGenericHook(hook: Hook): hook is GenericHook {
+  return hook.hookType === 'GENERIC_HOOK';
+}
+
+export function isSlackHook(hook: Hook): hook is SlackHook {
+  return hook.hookType === 'SLACK_HOOK';
+}
+
+export function isGithubHook(hook: Hook): hook is GithubHook {
+  return hook.hookType === 'GITHUB_STATUS_HOOK';
+}
+
+export function isBitbucketHook(hook: Hook): hook is BitBucketHook {
+  return hook.hookType === 'BITBUCKET_STATUS_HOOK';
+}
+
+export function isTeamsHook(hook: Hook): hook is TeamsHook {
+  return hook.hookType === 'TEAMS_HOOK';
+}
